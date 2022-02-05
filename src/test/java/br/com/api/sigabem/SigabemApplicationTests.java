388731093c4a72e775fee5074cbd1f91f41f6d23
Alexandre Faria <alexandre_faria123@icloud.com>
2022-02-04 21:53:08 -0300

    first commit

---
+++ src/test/java/br/com/api/sigabem/SigabemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.sigabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigabemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
