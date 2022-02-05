2204ca53f54d855cbc77628586f07239f5b0d8e4
Alexandre Faria <alexandre_faria123@icloud.com>
2022-02-04 22:13:19 -0300

    add dependencies swagger

---
+++ src/main/java/br/com/api/sigabem/SigabemApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.sigabem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigabemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigabemApplication.class, args);
+	}
+
+}
