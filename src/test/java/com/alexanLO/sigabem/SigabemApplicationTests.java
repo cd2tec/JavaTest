8d3784cb5f7a6bcb811463c30fb084b1936dc454
Alexan Lima <alexanlima1.0@gmail.com>
2022-07-10 15:42:13 -0300

    Projeto criado e dependencias importadas

---
+++ src/test/java/com/alexanLO/sigabem/SigabemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexanLO.sigabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigabemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
